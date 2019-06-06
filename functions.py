42ecf0ff34a3945d632b14945e019bb73b1ee022
kNosek1618 <k.nosek@ievent-management.pl>
2019-06-06 14:32:25 +0200

    extension of the function

--- functions.py
+++ functions.py
@@ -4,3 +4,23 @@ def say_hi():
     print("Hello user :)")
 
 say_hi()
+
+print("")
+#using the function
+def kind_of_car(name):
+    print("Name this car is " +name)
+
+kind_of_car("Ferrari")
+kind_of_car("Maserati")
+
+print("")
+#using the function 1.2
+def kind_of_car(name, HP):
+    print("Name this car is " +name+ " and he have " +str(HP)+ " HP.")
+
+kind_of_car("Ferrari", 605)
+kind_of_car("Maserati", 440)
+
+
+
+
